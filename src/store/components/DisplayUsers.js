047b024bc06f37dc871a936c5f46eb72b93e2906
Zilola-Nazarova <61951420+Zilola-Nazarova@users.noreply.github.com>
2023-08-24 02:02:50 +0500

    Dispatch getUsers action with useEffect hook, diplay content conditionally

--- src/store/components/DisplayUsers.js
+++ src/store/components/DisplayUsers.js
@@ -1,21 +1,34 @@
-import { useSelector } from 'react-redux/es/hooks/useSelector';
+import { useSelector, useDispatch } from 'react-redux';
+import { useEffect } from 'react';
+import { getUsers } from '../users/usersSlice';
 
 const DisplayUsers = () => {
+  const dispatch = useDispatch();
   const { users, isLoading, error } = useSelector((store) => store.users);
+  
+  useEffect(() => {
+    dispatch(getUsers());
+  }, [dispatch]);
+
+  // useEffect(() => {
+  //   dispatch(getUsers());
+  // }, [getUsers()]);
 
   return (
     <>
-      { isLoading && <p>The users list is loading!</p> }
-      { error && <p>Something went wrong!</p> }
-      <ul>
+      { isLoading === true ? (
+        <p>The users list is loading!</p>
+      ) : error ? (
+        <p>Something went wrong!</p>
+      ) : (
+        <ul>
         {users.map((user) => (
-          <li
-            key={user.id}
-          >
-            { user }
+          <li key={user.login.uuid}>
+            { `${user.name.first} ${user.name.last}` }
           </li>
         ))}
       </ul>
+      )}
     </>
   );
 };
