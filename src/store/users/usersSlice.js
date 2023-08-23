4bd792ca985527354c5fa65f8238a5e0594421c7
Zilola-Nazarova <61951420+Zilola-Nazarova@users.noreply.github.com>
2023-08-24 02:00:50 +0500

    Create an action creator that fetches users, add extraReducers

--- src/store/users/usersSlice.js
+++ src/store/users/usersSlice.js
@@ -1,4 +1,20 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+const url = 'https://randomuser.me/api/?results=5';
+
+export const getUsers = createAsyncThunk(
+  'users/getUsers',
+  async (thunkAPI) => {
+    try {
+      const resp = await axios.get(url);
+      return resp.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error);
+      // return error.message;
+    }
+  }
+);
 
 const initialState = {
   users: [],
@@ -10,9 +26,23 @@ export const usersSlice = createSlice({
   name: 'users',
   initialState,
   reducers: {},
-  extraReducers: {},
+  extraReducers(builder) {
+    builder
+      .addCase(getUsers.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(getUsers.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.users = action.payload.results;
+      })
+      .addCase(getUsers.rejected, (state, action) => {
+        state.isLoading = false;
+        state.error = action.payload.message;
+        // state.error = action.error.message;
+      })
+  },
 });
 
-export const {  } = usersSlice.actions;
+// export const { } = usersSlice.actions;
 
 export default usersSlice.reducer;
