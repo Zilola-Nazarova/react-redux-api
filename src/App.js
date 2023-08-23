4ace6311cdd3404081c729d13ef4aaeebf8fbd00
Zilola-Nazarova <61951420+Zilola-Nazarova@users.noreply.github.com>
2023-08-24 02:03:15 +0500

    Add DispayUsers component to App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import DisplayUsers from './store/components/DisplayUsers';
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <DisplayUsers />
     </div>
   );
 }
